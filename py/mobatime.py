c9222bb37159d0de4461e0adbab81dcc29afcdd6
chfo-bidouille <chfo@pm.me>
2026-04-25 21:27:07 +0200

    nettoyage

--- py/mobatime.py
+++ py/mobatime.py
@@ -20,19 +20,9 @@ ser = serial.Serial(
         )
 print(f"Connected to {ser.name}")
 
-
-# s1 = 0 # S1 = 0, pour comapraison avec S0
-
 while True:
-    #s0 = time.strftime("%-S")
     s0 = datetime.datetime.now() # defini S0 à l'heure actuelle
-    # print("s0 = ", s0.strftime("%S.%f"), ", s0 = ", s0) #debug
-    #print("s0 = ",s0.strftime("%-S.%f") ," s1 = ", s1) # debug
 
-    # si la seconde actuelle (s0)  n'est pas égale à la seconde enregistrée en fin
-    # de boucle (s1), la seconde à changé, on entre dans la boucle. Si la seconde est 
-    # identique, on ne refait pas un télegramme et on attends un dixième de seconde.
-    # if s0.strftime("%-S") != s1:
     # creation du telegrame
     tgrm = "OAL" + s0.strftime("%y%m%dF%H%M%S") + "\r"
 
@@ -44,8 +34,4 @@ while True:
 
     ser.write(tgrm_encoded) # envoyer sur port série
 
-    # s1 = s0.strftime("%-S") # defini s1 pour comparaison avec s0
     time.sleep(60) 
-    #else:
-    # time.sleep(.1) # attente un dixième de seconde
-
