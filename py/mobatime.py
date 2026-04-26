d65714bca7c1f1b3c234066126a9c11a9104c36c
chfo-bidouille <chfo@pm.me>
2026-04-26 18:48:38 +0200

    ajouter une attente de la seconde suivante sur la base des microsecondes

--- py/mobatime.py
+++ py/mobatime.py
@@ -10,7 +10,7 @@ import time
 import datetime
 import serial
 
-ser = serial.Serial(
+ser = serial.Serial( # ouvrir et configurer le port séri
         port='/dev/ttyAMA0', # /dev/ttyAMA0 ou /dev/ttyAMA10
         baudrate=9600,
         bytesize=serial.SEVENBITS,
@@ -20,15 +20,35 @@ ser = serial.Serial(
         )
 print(f"Connected to {ser.name}")
 
-s0 = datetime.datetime.now() # defini S0 à l'heure actuelle
+'''
+Attendre l'heure exacte 
+'''
+now = datetime.datetime.now() # heure actuelle exacte
+micro = int(now.strftime("%f")) # microseconde actuelle
+
+# print(micro)
+
+# combien de microsecondes à attendre avant la seconde suivante, converti en seconde, pour avoir "seconde,000000"
+wait = (1000000-micro) / 1000000
+
+# print(wait)
+
+time.sleep(wait) # attend le nombre de microsecondes nécessaire (converti en seconde)
+
+'''
+Création et envoi du télégramme
+'''
+h = datetime.datetime.now() # defini h à l'heure actuelle
 
 # creation du telegrame
-tgrm = "OAL" + s0.strftime("%y%m%dF%H%M%S") + "\r"
+tgrm = "OAL" + h.strftime("%y%m%dF%H%M%S") + "\r"
 
 tgrm_encoded = tgrm.encode(encoding='ascii') # encodage
 
-# tgrm_hex = tgrm_encoded.hex() #encodage pour debug
-print(datetime.datetime.now(), " : telegram ascii : ", tgrm_encoded) # affiche pour debug
-# print("telegram ascii : ", tgrm_encoded, ", hex : ", tgrm_hex) # affiche pour debug
-
 ser.write(tgrm_encoded) # envoyer sur port série
+
+# tgrm_hex = tgrm_encoded.hex() #encodage pour debug
+# print("telegram ascii : ", tgrm_encoded, ", hex : ", tgrm_hex) # affiche pour debug
+print(datetime.datetime.now(), " : telegram ascii : ", tgrm_encoded) # affiche le télégramme envoyé
+
+ser.close() # fermer le port série
